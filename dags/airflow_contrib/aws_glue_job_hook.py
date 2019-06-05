4f9c35998128c329462689e64a3c0e3676552676
Christopher Cato <christopher.cato@mittmedia.se>
2019-06-05 13:35:14 +0200

    [master]: Adding glue hooks

---
+++ dags/airflow_contrib/aws_glue_job_hook.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+#
+# This is copied from https://github.com/apache/airflow/pull/4068/files 2019-04-23 // Frans Krojegård
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+
+from airflow.exceptions import AirflowException
+from airflow.contrib.hooks.aws_hook import AwsHook
+import time
+from botocore.exceptions import ClientError
+
+
+class AwsGlueJobHook(AwsHook):
+    """
+    Interact with AWS Glue - create job
+
+    :param job_name: unique job name per AWS account
+    :type str
+    :param desc: job description
+    :type str
+    :param aws_conn_id: aws connection id
+    :type aws_conn_id: str
+    """
+
+    def __init__(self,
+                 job_name=None,
+                 desc=None,
+                 aws_conn_id='aws_default',
+                 *args, **kwargs):
+        self.job_name = job_name
+        self.desc = desc
+        self.aws_conn_id = aws_conn_id
+        super(AwsGlueJobHook, self).__init__(self.aws_conn_id, *args, **kwargs)
+
+    def get_conn(self):
+        return self.get_client_type('glue')
+
+    def list_jobs(self):
+        conn = self.get_conn()
+        return conn.get_jobs()
+
+    def initialize_job(self, script_args=None):
+        """
+        Initializes connection with AWS Glue
+        to run job
+        :return:
+        """
+        glue_client = self.get_conn()
+
+        try:
+            job_response = self.get_glue_job()
+            job_name = job_response['Name']
+            job_run = glue_client.start_job_run(
+                JobName=job_name,
+                Arguments=script_args
+            )
+            return self.job_completion(job_name, job_run['JobRunId'])
+        except ClientError as general_error:
+            raise AirflowException(
+                'Failed to run aws glue job, error: {error}'.format(
+                    error=str(general_error)
+                )
+            )
+
+    def job_completion(self, job_name=None, run_id=None):
+        """
+        :param job_name:
+        :param run_id:
+        :return:
+        """
+        glue_client = self.get_conn()
+
+        while True:
+
+            job_status = glue_client.get_job_run(
+                JobName=job_name,
+                RunId=run_id,
+                PredecessorsIncluded=True
+            )
+            job_run_state = job_status['JobRun']['JobRunState']
+
+            error_message = job_status['JobRun']['ErrorMessage'] if 'ErrorMessage' in job_status['JobRun'] else None
+
+            if job_run_state in {'FAILED', 'STOPPED', 'SUCCEEDED'}:
+                self.log.info("Exiting Job {} Run State: {}"
+                              .format(run_id, job_run_state))
+                return {'JobRunState': job_run_state, 'JobRunId': run_id, 'ErrorMessage': error_message}
+            else:
+                self.log.info("Polling for AWS Glue Job {} current run state"
+                              .format(job_name))
+                time.sleep(6)
+
+    def get_glue_job(self):
+        """
+        Function should return glue job details
+        :return:
+        """
+        try:
+            self.log.info(
+                "Retrieving AWS Glue Job: {job_name}"
+                    .format(job_name=self.job_name)
+            )
+            glue_client = self.get_conn()
+            get_job_response = glue_client.get_job(
+                JobName=self.job_name
+            )['Job']
+            self.logger.info("Found AWS Glue Job: {job_name}".format(job_name=self.job_name))
+            self.logger.info("Job Creation Time: {}".format(get_job_response['CreatedOn']))
+            self.logger.info("Last Job Modification Time: {}".format(get_job_response['LastModifiedOn']))
+            return get_job_response
+        except ClientError as general_error:
+            raise AirflowException(
+                'Failed to create aws glue job, error: {error}'.format(
+                    error=str(general_error)
+                )
+            )
---
+++ dags/airflow_contrib/aws_glue_job_operator.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+#
+# This is copied from https://github.com/apache/airflow/pull/4068/files 2019-04-23 // Frans Krojegård
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+from __future__ import unicode_literals
+
+from .aws_glue_job_hook import AwsGlueJobHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class AWSGlueJobOperator(BaseOperator):
+    """
+    Creates an AWS Glue Job. AWS Glue is a serverless Spark
+    ETL service for running Spark Jobs on the AWS cloud.
+    Language support: Python and Scala
+
+    :param job_name: unique job name per AWS Account
+    :type str
+    :param job_desc: job description details
+    :type str
+    :param script_args: etl script arguments and AWS Glue arguments
+    :type dict
+    :param aws_conn_id: aws connection id
+    :type aws_conn_id: str
+    """
+    template_fields = ()
+    template_ext = ()
+    ui_color = '#ededed'
+
+    @apply_defaults
+    def __init__(self,
+                 job_name='aws_glue_default_job',
+                 job_desc='AWS Glue Job with Airflow',
+                 script_args={},
+                 aws_conn_id='aws_default',
+                 *args, **kwargs
+                 ):
+        super(AWSGlueJobOperator, self).__init__(*args, **kwargs)
+        self.job_name = job_name
+        self.job_desc = job_desc
+        self.script_args = script_args
+        self.aws_conn_id = aws_conn_id
+
+    def execute(self, context):
+        """
+        Executes AWS Glue Job from Airflow
+        :return:
+        """
+        glue_job = AwsGlueJobHook(
+            job_name=self.job_name,
+            desc=self.job_desc,
+            aws_conn_id=self.aws_conn_id
+        )
+
+        self.script_args['--date'] = context.get('execution_date').strftime('%Y-%m-%d')
+
+        self.log.info("Initializing AWS Glue Job: {}".format(self.job_name))
+        glue_job_run = glue_job.initialize_job(self.script_args)
+
+        error_message = 'Error message: {}'.format(glue_job_run['ErrorMessage']) if glue_job_run['ErrorMessage'] else ''
+        self.log.info('AWS Glue Job: {job_name} status: {job_status}. '
+                      'Run Id: {run_id}{error_message}'
+                      .format(run_id=glue_job_run['JobRunId'],
+                              job_name=self.job_name,
+                              job_status=glue_job_run['JobRunState'],
+                              error_message=error_message)
+                      )
+        if glue_job_run['JobRunState'] != 'SUCCEEDED':
+            raise AirflowException('AWS Glue job failed with error: {}'
+                                   .format(glue_job_run['ErrorMessage']))
+
+        self.log.info('Done.')
